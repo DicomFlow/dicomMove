01c6fe9e8d268cd999a51ef8bda36941b82a17fb
netolucena <juracylucena@gmail.com>
2016-05-19 12:12:53 -0300

    .

--- src/br/ufpb/dicomflow/util/RequestOperations.java
+++ src/br/ufpb/dicomflow/util/RequestOperations.java
@@ -3,6 +3,8 @@ package br.ufpb.dicomflow.util;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Request;
 
+import br.ufpb.dicomflow.ws.CommonRequest;
+
 public class RequestOperations {
 	
 	public static final String contentMD5Header = "Content-MD5";
@@ -18,6 +20,18 @@ public class RequestOperations {
 				+ canonicalizeHeaders + 
 				canonicalizedResource;					
 		return signature;
+	}
+	
+	public static String getHeaderString(CommonRequest objectRequest, Request request) {
+		//TODO canonicalizeHeaders and canonicalizedResource  		
+		String canonicalizeHeaders = "";
+		String canonicalizedResource = "";
+		String signature = request.getMethod() + "\n" 
+				+ objectRequest.getContentMD5() + "\n" 
+				+ objectRequest.getDate() + "\n"
+				+ canonicalizeHeaders + 
+				canonicalizedResource;		
+		return signature.toString();
 	}
 			
 }
