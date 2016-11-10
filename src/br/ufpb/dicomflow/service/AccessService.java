3a7e6045f648c06b118346e7cf88a7326fa8b3b0
daniloalexandre <daniloalexandre@sti.ufpb.br>
2016-11-10 12:21:49 -0300

    ajustes na documentação das classes

--- src/br/ufpb/dicomflow/service/AccessService.java
+++ src/br/ufpb/dicomflow/service/AccessService.java
@@ -15,33 +15,10 @@
  * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
-package br.ufpb.dicomflow.ws;
+package br.ufpb.dicomflow.service;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-@Path("/hello")
-public class Hello {
-
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String sayPlainTextHello() {
-		return "Hello Jersey";
-	}
-
-	@GET
-	@Produces(MediaType.TEXT_XML)
-	public String sayXMLHello() {
-		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
-	}
-
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String sayHtmlHello() {
-		return "<html> " + "<title>" + "Hello Jersey" + "</title>"
-				+ "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
-	}
+public class AccessService {
 	
+	String regex = "[A-Za-z0-9\\._-]+@[A-Za-z0-9]+(\\.[A-Za-z]+)*#[A-Za-z0-9]+(\\.[A-Za-z0-9]+)+#[0-9]+#([Sharing|Storage|Request|Find|Discovery] *|([A-Za-z]{2}(,[A-Za-z]{2})+)+";
+
 }
--- src/br/ufpb/dicomflow/service/PersistentService.java
+++ src/br/ufpb/dicomflow/service/PersistentService.java
@@ -1,3 +1,20 @@
+/*
+ * 	This file is part of DicomFlow.
+ * 
+ * 	DicomFlow is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ * 
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ * 
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package br.ufpb.dicomflow.service;
 
 import java.io.Serializable;
@@ -326,7 +343,7 @@ String query = "from " + type.getName() + " type1 ";
 	
 	/**
 	 * Metodo que prepara uma query de busca em formato String 
-     * @param param o campo que será comparado na busca
+     * @param param o campo que serï¿½ comparado na busca
      * @param values os valores a serem comparados na busca
      * @param type o tipo da classe onde vai ser buscado os resultados
      * @return
@@ -717,7 +734,7 @@ String query = "from " + type.getName() + " type1 ";
 		if(values == null || values.isEmpty()){
 			return;
 		}
-		System.err.println("entrei no método que vai remover todos os registros selecionados!!!!!");
+		System.err.println("entrei no mï¿½todo que vai remover todos os registros selecionados!!!!!");
 		Session session = getSession();	
 		Transaction trans = null;
 		try {
@@ -1219,7 +1236,7 @@ String query = "from " + type.getName() + " type1 ";
 	
 	/**
 	 * Metodo que prepara uma query de busca em formato String 
-     * @param param o campo que será comparado na busca
+     * @param param o campo que serï¿½ comparado na busca
      * @param values os valores a serem comparados na busca
      * @param type o tipo da classe onde vai ser buscado os resultados
      * @return
